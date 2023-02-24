380d4f6b9d9e8d622be092afd1603c085742d3ef
WeiZheTseng <WeiZheTseng@gmail.com>
2023-02-24 17:42:15 +0800

    224

--- src/views/MemberCenter.vue
+++ src/views/MemberCenter.vue
@@ -7,27 +7,13 @@
                 <h2>會員專區</h2>
                 <p>Members Only</p>
             </div>
-            <div class="row">
-                <div class="col-3">
-                    <ul>
-                        <li>
-                            <RouterLink to="/memberCenter/orderSearch">查詢訂單</RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink to="/memberCenter/updateUser">修改會員資料</RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink to="/memberCenter/changePW">變更會員密碼</RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink to="/memberCenter/bindOther">綁定其他帳號</RouterLink>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-9">
-                    <RouterView />
-                </div>
+            <div v-if="route.path === '/memberCenter'">
+                <RouterLink class="btn btn-success" to="/memberCenter/orderSearch">查詢訂單</RouterLink>
+                <RouterLink class="btn btn-warning" to="/memberCenter/updateUser">修改會員資料</RouterLink>
+                <RouterLink class="btn btn-danger" to="/memberCenter/changePW">變更會員密碼</RouterLink>
+                <RouterLink class="btn btn-primary" to="/memberCenter/bindOther">綁定其他帳號</RouterLink>
             </div>
+            <RouterView />
         </div>
     </div>
 </template>
@@ -35,6 +21,9 @@
 <script setup>
 import pageBanner from '../components/btn_pageBanner.vue'
 import breadcrumb from '../components/btn_breadcrumb.vue'
+import { useRoute } from 'vue-router';
+import { onMounted } from 'vue';
+const route = useRoute()
 
 
 </script>
--- src/views/UpdateUser.vue
+++ src/views/UpdateUser.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>修改會員資料</h1>
+    <div class="row">
+        <div class="col-3">
+            <ul>
+                <li>
+                    <RouterLink to="/memberCenter/orderSearch">查詢訂單</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/memberCenter/updateUser">修改會員資料</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/memberCenter/changePW">變更會員密碼</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/memberCenter/bindOther">綁定其他帳號</RouterLink>
+                </li>
+            </ul>
+        </div>
+        <div class="col-9">
+            <RouterView />
+        </div>
+    </div>
 </template>
