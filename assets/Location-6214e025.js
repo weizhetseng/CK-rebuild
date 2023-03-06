554e5ae3ef1dbfe663392540978ee9ee2faaa607
WeiZheTseng <WeiZheTseng@gmail.com>
2023-03-06 13:37:35 +0800

    deploy

--- assets/Location-6214e025.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as o}from"./btn_pageBanner-18f532ef.js";import{o as t,c,b as a,a as s,F as n}from"./index-5c462238.js";const _={class:"wrap"},i={class:"container location"},r=s("div",{class:"pageTitle"},[s("h2",null,"營業據點"),s("p",null,"Business base")],-1),f={__name:"Location",setup(l){return(m,p)=>(t(),c(n,null,[a(e),s("div",_,[s("div",i,[a(o),r])])],64))}};export{f as default};
